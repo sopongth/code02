4101dc41fd5e4ebfeb9a841f1fe401592f17065a
sopongth <104857667+sopongth@users.noreply.github.com>
2022-05-10 18:00:27 +0700

    update pysl054.py

--- PYSL054.py
+++ PYSL054.py
@@ -4,5 +4,5 @@ from streamlit_webrtc import webrtc_streamer
 st.title("ทดสอบกล้อง")
 
 webrtc_streamer(key="test",
-                media_stream_constraints={"video": True,
-                                          "audio": False})
+                media_stream_constraints={"video": True,"audio": False},
+                rtc_configuration={"iceServers":[{"urls",:["stun:stun.l.google.com:19302"]}])}
