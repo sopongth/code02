bb5870ddb8bbe985d96fd1ee267a386fa2d716c6
sopongth <104857667+sopongth@users.noreply.github.com>
2022-05-10 18:02:59 +0700

    update pysl054.py v3

--- PYSL054.py
+++ PYSL054.py
@@ -5,4 +5,4 @@ st.title("ทดสอบกล้อง")
 
 webrtc_streamer(key="test",
                 media_stream_constraints={"video": True,"audio": False},
-                rtc_configuration={"iceServers":[{"urls":["stun:stun.l.google.com:19302"]}])}
+                rtc_configuration={"iceServers":[{"urls":["stun:stun.l.google.com:19302"]}]})
