9004b37ef065ed05cf6bd149bf0df0ab3d2bb17f
sopongth <104857667+sopongth@users.noreply.github.com>
2022-05-10 17:52:43 +0700

    create pysl054

---
+++ PYSL054.py
@@ -0,0 +1,8 @@
+import streamlit as st
+from streamlit_webrtc import webrtc_streamer
+
+st.title("ทดสอบกล้อง")
+
+webrtc_streamer(key="test",
+                media_stream_constraints={"video": True,
+                                          "audio": False})
